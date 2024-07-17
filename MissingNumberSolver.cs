8e6eb604a8b34644d1b495d869fabb904228ba41
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-07-17 17:34:19 +0300

    ready task

---
+++ MissingNumberSolver.cs
@@ -0,0 +1,17 @@
+﻿namespace MissingNumber;
+
+public class MissingNumberSolver
+{
+    public static int FindMissingNumber(int[] nums)
+    {
+        var maxNumber = nums.Max();
+
+        var sumSequence = maxNumber * (maxNumber + 1) / 2;
+
+        var sumNums = nums.Sum();
+
+        var missingNumber = sumSequence - sumNums;
+
+        return missingNumber;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MissingNumber;
+
+public class Programm
+{
+    public static void Main()
+    {
+        var missingNumber = MissingNumberSolver.FindMissingNumber([0,3,1,2,5,6,7,9,4]);
+
+        Console.WriteLine(missingNumber);
+    }
+}
